cd532c1404ebefad190b98a63c9aa5272959ab9a
unknown <caoxiaolin@rong360ad.com>
2018-09-21 10:06:16 +0800

    modify by caoxiaolin

--- stack/arithmetic.go
+++ stack/arithmetic.go
@@ -2,138 +2,140 @@
 package main
 
 import (
-    "fmt"
-    "strconv"
+	"fmt"
+	"strconv"
 )
 
-type Arithmetic struct{
-    Input string  //原始输入
-    MiddleExpression []string   //中序表达式
-    SuffixExpression []string   //后序表达式
+type Arithmetic struct {
+	Input            string   //原始输入
+	MiddleExpression []string //中序表达式
+	SuffixExpression []string //后序表达式
 }
 
 //规范表达式，处理多位数，输出到中序表达式
-func (a *Arithmetic) Format(){
-    if a.Input != ""{
-        strlen := len(a.Input)
-        var str string
-        for i := 0; i < strlen; i++{
-            c := a.Input[i]
-            s := string(c)
-            if a.IsNumber(c){
-                str += s
-                if i + 1 < strlen && a.IsNumber(a.Input[i+1]){
-                    continue
-                }
-            }else{
-                if str != ""{
-                    a.MiddleExpression = append(a.MiddleExpression, str)
-                }
-                a.MiddleExpression = append(a.MiddleExpression, s)
-                str = ""
-            }
-            if i + 1 == strlen && str != ""{
-                a.MiddleExpression = append(a.MiddleExpression, str)
-            }
-        }
-    }
+func (a *Arithmetic) Format() {
+	if a.Input != "" {
+		strlen := len(a.Input)
+		var str string
+		for i := 0; i < strlen; i++ {
+			c := a.Input[i]
+			s := string(c)
+			//如果是数字，则继续往后判断
+			if a.IsNumber(c) {
+				str += s //多位数连接
+				if i+1 < strlen && a.IsNumber(a.Input[i+1]) {
+					continue
+				}
+			} else {
+				if str != "" {
+					a.MiddleExpression = append(a.MiddleExpression, str)
+				}
+				a.MiddleExpression = append(a.MiddleExpression, s)
+				str = ""
+			}
+			if i+1 == strlen && str != "" {
+				a.MiddleExpression = append(a.MiddleExpression, str)
+			}
+		}
+	}
 }
 
 //转为逆波兰表达式（后缀表达式）
 //规则：顺序遍历，数字输出，符号则判断与栈顶符号的优先级，右括号或优先级不高于栈顶符号
 //栈顶元素依次出栈，遇左括号停止，当前符号进栈，最后都出栈
-func (a *Arithmetic) RPN(){
-    if a.Input != ""{
-        a.Format()
-        stack := InitStack()
-        for _, s := range a.MiddleExpression{
-            //fmt.Printf("当前 s = %v\n", s)
-            //转换为int，如果转换失败，说明是括号或操作符
-            _, err := strconv.Atoi(s)
-            if err != nil{  //转换失败
-                    top, _ := stack.GetTop()
-                    //s为"("时，直接入栈
-                    if s == "("{
-                        stack.Push(s)
-                        continue
-                    }
-                    //s为右括号，或者s运算符的优先级不高于栈顶运算符优先级
-                    for top != nil && (s == ")" || top.(string) == "*" || top.(string) == "/"){
-                        //栈顶元素开始出栈，直到"("出栈为止
-                        e, _ := stack.Pop()
-                        v, _ := e.(string)
-                        if v == "("{
-                            break
-                        }
-                        
-                        a.SuffixExpression = append(a.SuffixExpression, v)
-                        top, _ = stack.GetTop()
-                    }
-                    //当前符号进栈
-                    if s != ")"{
-                        stack.Push(s)
-                    }
-            }else{
-                a.SuffixExpression = append(a.SuffixExpression, s)
-            }
-        }
+func (a *Arithmetic) RPN() {
+	if a.Input != "" {
+		a.Format()
+		stack := InitStack()
+		for _, s := range a.MiddleExpression {
+			//fmt.Printf("当前 s = %v\n", s)
+			//转换为int，如果转换失败，说明是括号或操作符
+			_, err := strconv.Atoi(s)
+			if err != nil { //转换失败
+				top, _ := stack.GetTop()
+				//s为"("时，直接入栈
+				if s == "(" {
+					stack.Push(s)
+					continue
+				}
+				//s为右括号，或者s运算符的优先级不高于栈顶运算符优先级
+				for top != nil && (s == ")" || top.(string) == "*" || top.(string) == "/") {
+					//栈顶元素开始出栈，直到"("出栈为止
+					e, _ := stack.Pop()
+					v, _ := e.(string)
+					if v == "(" {
+						break
+					}
+					a.SuffixExpression = append(a.SuffixExpression, v)
+					top, _ = stack.GetTop()
+				}
+				//当前符号进栈
+				if s != ")" {
+					stack.Push(s)
+				}
+			} else {
+				//数字直接入栈
+				a.SuffixExpression = append(a.SuffixExpression, s)
+			}
+		}
 
-        //如果栈不为空，则元素全部出栈
-        for stack.IsEmpty() != true{
-            e, _ := stack.Pop()
-            a.SuffixExpression = append(a.SuffixExpression, e.(string))
-        }
-    }
+		//如果栈不为空，则元素全部出栈
+		for stack.IsEmpty() != true {
+			e, _ := stack.Pop()
+			a.SuffixExpression = append(a.SuffixExpression, e.(string))
+		}
+	}
 }
 
 //计算最终结果
-func (a *Arithmetic) CalcResult() float64{
-    if a.Input != ""{
-        a.RPN()
-        stack := InitStack()
-        for _, s := range a.SuffixExpression{
-            val, err := strconv.ParseFloat(s, 64)
-            if err == nil{
-                stack.Push(val)
-            }else{
-                n, _ := stack.Pop()
-                m, _ := stack.Pop()
-                stack.Push(a.Calc(m.(float64), n.(float64), s))
-            }
-        }
-        res, _ := stack.Pop()
-        return res.(float64)
-    }
-    return 0
+//数字挨个入栈，遇到符号，前两个数出栈并计算，结果入栈，直到计算出最终结果出栈
+func (a *Arithmetic) CalcResult() float64 {
+	if a.Input != "" {
+		a.RPN()
+		stack := InitStack()
+		for _, s := range a.SuffixExpression {
+			val, err := strconv.ParseFloat(s, 64)
+			if err == nil {
+				stack.Push(val)
+			} else {
+				n, _ := stack.Pop()
+				m, _ := stack.Pop()
+				stack.Push(a.Calc(m.(float64), n.(float64), s))
+			}
+		}
+		res, _ := stack.Pop()
+		return res.(float64)
+	}
+	return 0
 }
 
 //判断是否数字
-func (a *Arithmetic) IsNumber(c byte) bool{
-    if c >= '0' && c <= '9'{
-        return true
-    }else{
-        return false
-    }
+func (a *Arithmetic) IsNumber(c byte) bool {
+	if c >= '0' && c <= '9' {
+		return true
+	} else {
+		return false
+	}
 }
 
 //基本的加减乘除四则运算
-func (a *Arithmetic) Calc(x float64, y float64, o string) (res float64){
-    switch(o){
-    case "+":
-        res = x+y
-    case "-":
-        res = x-y
-    case "*":
-        res = x*y
-    case "/":
-        res = x/y
-    }
-    //fmt.Printf("%v %v %v = %v\n", x, o, y, res)
-    return
+func (a *Arithmetic) Calc(x float64, y float64, o string) (res float64) {
+	switch o {
+	case "+":
+		res = x + y
+	case "-":
+		res = x - y
+	case "*":
+		res = x * y
+	case "/":
+		res = x / y
+	}
+	//fmt.Printf("%v %v %v = %v\n", x, o, y, res)
+	return
 }
 
-func main(){
-    a := new(Arithmetic)
-    a.Input = "11+2*(42-3)/5*((7-6)/8*9)"
-    fmt.Printf("%s = %f\n", a.MiddleExpression, a.CalcResult())
+func main() {
+	a := new(Arithmetic)
+	a.Input = "11+2*(42-3)/5*((7-6)/8*9)"
+	fmt.Printf("%s = %f\n", a.MiddleExpression, a.CalcResult())
 }
--- stack/stack.go
+++ stack/stack.go
@@ -3,28 +3,28 @@
 package main
 
 import (
-	"fmt"
 	"errors"
+	"fmt"
 )
 
 //栈的长度
 const SIZE = 10
 
-type Stack struct{
+type Stack struct {
 	Data [SIZE]interface{}
-	Top int
+	Top  int
 }
 
 //初始化空栈，空栈Top=-1
-func InitStack() *Stack{
+func InitStack() *Stack {
 	s := new(Stack)
 	s.Top = -1
 	return s
 }
 
 //入栈
-func (s *Stack) Push(e interface{}) error{
-	if s.Top == SIZE -1{
+func (s *Stack) Push(e interface{}) error {
+	if s.Top == SIZE-1 {
 		return errors.New("stack is full")
 	}
 	s.Top++
@@ -34,8 +34,8 @@ func (s *Stack) Push(e interface{}) error{
 }
 
 //出栈
-func (s *Stack) Pop() (interface{}, error){
-	if s.Top == -1{
+func (s *Stack) Pop() (interface{}, error) {
+	if s.Top == -1 {
 		return nil, errors.New("stack is empty")
 	}
 	r := s.Data[s.Top]
@@ -46,43 +46,43 @@ func (s *Stack) Pop() (interface{}, error){
 }
 
 //获取栈顶元素
-func (s *Stack) GetTop() (interface{}, error){
-	if s.Top == -1{
+func (s *Stack) GetTop() (interface{}, error) {
+	if s.Top == -1 {
 		return nil, errors.New("stack is empty")
 	}
 	return s.Data[s.Top], nil
 }
 
 //是否空栈
-func (s *Stack) IsEmpty() bool{
-	if s.Top == -1{
+func (s *Stack) IsEmpty() bool {
+	if s.Top == -1 {
 		return true
 	}
 	return false
 }
 
 //输出栈内元素
-func (s *Stack) Print(){
-	for i:=0; i<=s.Top;i++{
+func (s *Stack) Print() {
+	for i := 0; i <= s.Top; i++ {
 		fmt.Printf("%v\t", s.Data[i])
 	}
 	fmt.Println()
 }
 
-func Example(){
+func Example() {
 	stack := InitStack()
-	for i:=1; i<=10; i++{
+	for i := 1; i <= 10; i++ {
 		stack.Push(i)
 	}
 	err1 := stack.Push(11)
-	if err1 != nil{
+	if err1 != nil {
 		fmt.Println(err1)
 	}
 	stack.Print()
 	data, err2 := stack.Pop()
-	if err2 != nil{
+	if err2 != nil {
 		fmt.Println(err2)
-	}else{
+	} else {
 		fmt.Printf("Pop element %v\n", data)
 	}
 	stack.Print()
